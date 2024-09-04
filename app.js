61b0cac7d79d7c790553f91180e63bfe24c950ba
kevinjamiesondev <kevinjamiesondev@gmail.com>
2024-09-04 13:48:39 -0500

    Added new files

---
+++ app.js
@@ -0,0 +1,27 @@
+// Set initial count
+let count = 0;
+
+
+// Select value and buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+     const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count--;
+        } else if(styles.contains('increase')){
+            count++;
+        } else {
+            count = 0;
+        } if (count > 0) {
+            value.style.color = "lightgreen";
+        } if (count < 0) {
+            value.style.color = "red";
+        } if (count === 0) {
+            value.style.color = "var(--clr-grey-1)"
+        }
+        value.textContent = count;
+    });
+});
